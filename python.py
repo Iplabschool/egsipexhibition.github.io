5874ab9d35fa7fb550fff1346f0139bed4e739b4
Iplabschool <117159721+Iplabschool@users.noreply.github.com>
2022-11-09 14:32:21 +0530

    Aa

--- python.py
+++ python.py
@@ -75,4 +75,8 @@ else :
     min, mid, max = c,b,a
 print("numbers in ascending order are : ",min, mid, max)
 
+#Volume of sphere
+r=float(input("Enter the radius of sphere:"))
+v=4*3.14*r*r*r/3
+print("Volume of sphere is",v)
 
